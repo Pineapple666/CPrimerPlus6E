7bf504886e7d8989760c38a9482144eeabeefceb
Pineapple666 <pineapple_cpp@163.com>
2021-06-09 00:08:31 +0800

    learn string input and output

---
+++ ch11/ex11_3_addresses.c
@@ -0,0 +1,15 @@
+#define MSG "I'am special"
+#include <stdio.h>
+
+int main(void)
+{
+    char ar[] = MSG;
+    const char *pt = MSG;
+    printf("address of \"I'am special\": %p \n", "I'am special");
+    printf("               address ar: %p\n", ar);
+    printf("               address pt: %p\n", pt);
+    printf("           address of MSG: %p\n", MSG);
+    printf("address of \"I'am special\": %p \n", "I'am special");
+
+    return 0;
+}
