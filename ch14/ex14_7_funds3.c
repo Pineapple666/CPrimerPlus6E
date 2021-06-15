38774f3c741b514cd145f07bf8975489abd9e39a
Pineapple666 <pineapple_cpp@163.com>
2021-06-15 10:29:51 +0800

    结构的成员，结构指针，结构作为函数参数

---
+++ ch14/ex14_7_funds3.c
@@ -0,0 +1,32 @@
+// funds3.c -- 传递结构
+#include <stdio.h>
+#define FUNDLEN 50
+
+struct funds
+{
+    char bank[FUNDLEN];
+    double bankfund;
+    char save[FUNDLEN];
+    double savefund;
+};
+
+double sum(struct funds); // 参数是一个结构
+
+int main(void)
+{
+    struct funds stan = {"Garlic-Melon Bank",
+                         4032.27,
+                         "Lucky's Savings and Loan",
+                         8543.94};
+    printf("Stan has a total of %.2f.\n", sum(stan));
+
+    return 0;
+}
+
+/**
+* moolah 是 stan 的一个副本，不会改变stan
+**/
+double sum(struct funds moolah)
+{
+    return (moolah.bankfund + moolah.savefund);
+}
